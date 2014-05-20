c2f63d336963951ca62650ac3c310da1a5ea0c12
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-05-20 23:01:04 +0530

    Don't elongate the document unnessarily

--- static/js/init.js
+++ static/js/init.js
@@ -86,7 +86,7 @@ $(function() {
 				if (skel.isActive('mobile') || skel.isActive('narrower'))
 					$sc.css('min-height', '0').css('height', 'auto');
 				else
-					$sc.css('min-height', $window.height()).css('height', $document.height());
+					$sc.css('min-height', $window.height());/*.css('height', $document.height());*/
 			}, 100);
 		}).trigger('resize');
 		
